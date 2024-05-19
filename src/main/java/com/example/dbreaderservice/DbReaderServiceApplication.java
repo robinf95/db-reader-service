1b3a14082e6e3832ef7152100570ee628f4e07e9
Robin Friedmann <robinfriedmann.rf@gmail.com>
2024-05-19 08:14:57 +0200

    initial commit

---
+++ src/main/java/com/example/dbreaderservice/DbReaderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbreaderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbReaderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbReaderServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dbreaderservice/DbReaderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbreaderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbReaderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
